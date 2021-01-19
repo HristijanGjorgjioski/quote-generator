6fd182d3630b558bbce6c1f89b744aad34e1f94c
HristijanGjorgjioski <hristijangorgioski503@gmail.com>
2021-01-19 16:53:08 +0100

    in progress..

--- script.js
+++ script.js
@@ -1,21 +1,28 @@
 let quote = document.querySelector(".cards__quote");
-const cards = document.querySelectorAll(".cards__single");
+const card = document.querySelector(".cards__single");
 
 function flipCard() {
   this.classList.toggle("flip");
-}
+};
 
-cards.forEach((card) => card.addEventListener("click", flipCard));
+const changePosition = () => {
+  card.className = "cards__single class-to-left";
+};
 
 fetch("https://type.fit/api/quotes")
   .then(function (response) {
     return response.json();
   })
-  .then(function (data) {
+  // .then(function (data) {
     // console.log(data);
-    cards.forEach((card) => card.addEventListener("click", () => {
-      let randomNumber = Math.floor(Math.random() * 501);
-      quote.innerHTML = data[randomNumber].text;
-    }));
-  });
+    //cards.forEach((card) => 
+    // cards.addEventListener("click", () => {
+    //   let randomNumber = Math.floor(Math.random() * 501);
+    //   quote.innerHTML = data[randomNumber].text;
+    // });
+  // }
 
+// cards.forEach((card) => card.addEventListener("click", flipCard));
+// card.addEventListener("click", flipCard);
+card.addEventListener("click", changePosition);
+// cards.forEach((card) => card.addEventListener("dblclick", changePosition));
